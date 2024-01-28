be9fff68991854bbad421301a561c1ce5314045b
Iamhhp <Iamhhp2020@gmail.com>
2024-01-28 20:46:01 +0330

    add animation by package aos and countUp

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,35 +1,46 @@
-import { memo } from 'react';
 import './Hero.css';
+import { memo, useEffect } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import imgHero from '../../assets/images/hero.svg';
 import { FaGooglePlay, FaRProject } from 'react-icons/fa';
 import { PiStudentFill } from 'react-icons/pi';
 import { RiArticleFill } from 'react-icons/ri';
 import { GiTeacher } from 'react-icons/gi';
+import aos from 'aos';
+import 'aos/dist/aos.css';
+import CountUp from 'react-countup';
 
 const Hero = () => {
+  useEffect(() => {
+    console.log('Hero reRender!');
+  });
+
+  useEffect(() => {
+    aos.init();
+  }, []);
+
   const dataHeroBox = [
-    { title: 'تعداد دانشجوها', icon: <PiStudentFill className='icon' />, count: 1072 },
-    { title: 'تعداد پروژه ها', icon: <FaRProject className='icon' />, count: 76 },
-    { title: 'تعداد مقالات', icon: <RiArticleFill className='icon' />, count: 14 },
-    { title: 'تعداد دوره ها', icon: <GiTeacher className='icon' />, count: 11 },
+    { id: 0, title: 'تعداد دانشجوها', icon: <PiStudentFill className='icon' />, count: 1072 },
+    { id: 1, title: 'تعداد پروژه ها', icon: <FaRProject className='icon' />, count: 76 },
+    { id: 2, title: 'تعداد مقالات', icon: <RiArticleFill className='icon' />, count: 14 },
+    { id: 3, title: 'تعداد دوره ها', icon: <GiTeacher className='icon' />, count: 11 },
   ];
 
   return (
     <Container fluid className='container-hero'>
       <Container>
         <Row>
-          <Col className='sec-r col-12 col-md-5 col-lg-5 col-xl-6'>
+          <Col className='sec-r col-12 col-md-5 col-lg-5' data-aos='fade-left'>
             <img src={imgHero} alt='Hero images' />
           </Col>
 
-          <Col className='sec-l col-12 col-md-7 col-lg-7 col-xl-6'>
+          <Col className='sec-l col-12 col-md-7 col-lg-7' data-aos='fade-right'>
             <div className='title-hero'>آمارها باعث افتخار ما هستند!</div>
 
             <Row className='row-cols-1 row-cols-sm-2'>
               {dataHeroBox.map((data) => {
                 return (
-                  <Col>
+                  <Col data-aos='flip-right' key={data.id}>
                     <HeroBox {...data} />
                   </Col>
                 );
@@ -53,7 +64,7 @@ const HeroBox = ({ icon, title, count }) => {
       <div className='title'>
         {icon} {title}
       </div>
-      <div className='count'>{count}</div>
+      <CountUp className='count' delay={1} end={count} />
     </Container>
   );
 };
