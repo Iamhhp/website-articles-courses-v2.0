9a6e976dd0d46077bb219704d4b29b90973006aa
Iamhhp <Iamhhp2020@gmail.com>
2024-01-28 14:58:16 +0330

    create offcanvas menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,10 @@ import { Col, Row, Container } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
 import { SlBasket } from 'react-icons/sl';
 import { FaChevronUp } from 'react-icons/fa';
-import { memo, useEffect, useRef } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 import persianDate from 'persian-date';
+import OffCanVas from '../OffCanVas/OffCanVas';
+import { AiOutlineMenuFold } from 'react-icons/ai';
 
 const Header = () => {
   useEffect(() => {
@@ -28,54 +30,67 @@ const Header = () => {
     };
   }, []);
 
+  const itemsHeaderMenu = (
+    <ul>
+      <li>
+        <NavLink to='/Home' className='active'>
+          خانه
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink to='/Courses'>دوره ها</NavLink>
+      </li>
+
+      <li className='drop-down-menu'>
+        <NavLink to='/Articles' className='main-menu'>
+          مقالات
+          <FaChevronUp className='icon' />
+        </NavLink>
+
+        <div className={'sub-menu'}>
+          <Link to={''}>ایجاد مقاله</Link>
+        </div>
+      </li>
+
+      <li>
+        <NavLink to='/About-us'>درباره ما</NavLink>
+      </li>
+    </ul>
+  );
+
+  const [isShowMenu, setIsShowMenu] = useState(false);
+  const iconOffMenuClickHandler = () => {
+    setIsShowMenu(true);
+  };
+
   return (
-    <Container fluid>
-      <Row className='container-header'>
-        <Col className='sec-r col-auto'>
-          <ul>
-            <li>
-              <NavLink to='/Home' className='active'>
-                خانه
-              </NavLink>
-            </li>
+    <>
+      {isShowMenu && <OffCanVas title={'منو اصلی'} itemsMenu={itemsHeaderMenu} stateShowMenu={{ isShowMenu, setIsShowMenu }} />}
 
-            <li>
-              <NavLink to='/Courses'>دوره ها</NavLink>
-            </li>
+      <Container fluid>
+        <Row className='container-header'>
+          <Col className='sec-r col-auto'>
+            {itemsHeaderMenu} <AiOutlineMenuFold className='icon-OffCanVas' onClick={iconOffMenuClickHandler} />
+          </Col>
 
-            <li className='drop-down-menu'>
-              <NavLink to='/Articles' className='main-menu'>
-                مقالات
-                <FaChevronUp className='icon' />
-              </NavLink>
+          <Col className='sec-m col-auto'>
+            <div className='lbl-website'>وب سایت سایت آموزشی پژوهشی</div>
+            <div className='date' ref={elementDate}>
+              <div className='day'>شنبه</div>
+              <div className='time'>17:22:22</div>
+            </div>
+          </Col>
 
-              <div className={'sub-menu'}>
-                <Link to={''}>ایجاد مقاله</Link>
-              </div>
-            </li>
-
-            <li>
-              <NavLink to='/About-us'>درباره ما</NavLink>
-            </li>
-          </ul>
-        </Col>
-
-        <Col className='sec-m col-auto'>
-          <div className='lbl-website'>وب سایت سایت آموزشی پژوهشی</div>
-          <div className='date' ref={elementDate}>
-            <div className='day'>شنبه</div>
-            <div className='time'>17:22:22</div>
-          </div>
-        </Col>
-
-        <Col className='sec-l col-auto'>
-          <button className='btn-login'>ورود</button>
-          <button className='btn-basket'>
-            <SlBasket className='icon' />
-          </button>
-        </Col>
-      </Row>
-    </Container>
+          <Col className='sec-l col-auto'>
+            <button className='btn-login'>ورود</button>
+            <button className='btn-basket'>
+              <SlBasket className='icon' />
+            </button>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 export default memo(Header);
---
+++ src/components/OffCanVas/OffCanVas.jsx
@@ -0,0 +1,33 @@
+import { memo, useEffect, useRef } from 'react';
+import './OffCanVas.css';
+
+const OffCanVas = ({ itemsMenu, title, stateShowMenu }) => {
+  useEffect(() => {
+    console.log('OffCanVas reRender!');
+  });
+
+  const elementContainerMenu = useRef(null);
+  useEffect(() => {
+    window.setTimeout(() => {
+      elementContainerMenu.current.classList.remove('container-OffCanVas-hide');
+    }, 50);
+  }, [stateShowMenu.isShowMenu]);
+
+  const showMenuHandler = () => {
+    elementContainerMenu.current.classList.add('container-OffCanVas-hide');
+
+    window.setTimeout(() => {
+      stateShowMenu.setIsShowMenu(false);
+    }, 500);
+  };
+
+  return (
+    <div className='container-OffCanVas container-OffCanVas-hide' ref={elementContainerMenu} onClick={showMenuHandler}>
+      <div className='menu'>
+        <div className='title'>{title}</div>
+        <div className='items'> {itemsMenu}</div>
+      </div>
+    </div>
+  );
+};
+export default memo(OffCanVas);
