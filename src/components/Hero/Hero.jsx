7107f742fb7394fcaa6fe6d6e8f551026fa44539
Iamhhp <Iamhhp2020@gmail.com>
2024-01-29 10:35:18 +0330

    add svg wave to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,33 +27,41 @@ const Hero = () => {
   ];
 
   return (
-    <Container fluid className='container-hero'>
-      <Container>
-        <Row>
-          <Col className='sec-r col-12 col-md-5 col-lg-5' data-aos='fade-left'>
-            <img src={imgHero} alt='Hero images' />
-          </Col>
+    <>
+      <Container fluid className='container-hero'>
+        <Container>
+          <Row>
+            <Col className='sec-r col-12 col-md-5 col-lg-5' data-aos='fade-left'>
+              <img src={imgHero} alt='Hero images' />
+            </Col>
 
-          <Col className='sec-l col-12 col-md-7 col-lg-7' data-aos='fade-right'>
-            <div className='title-hero'>آمارها باعث افتخار ما هستند!</div>
+            <Col className='sec-l col-12 col-md-7 col-lg-7' data-aos='fade-right'>
+              <div className='title-hero'>آمارها باعث افتخار ما هستند!</div>
 
-            <Row className='row-cols-1 row-cols-sm-2'>
-              {dataHeroBox.map((data) => {
-                return (
-                  <Col data-aos='flip-right' key={data.id}>
-                    <HeroBox {...data} />
-                  </Col>
-                );
-              })}
-            </Row>
+              <Row className='row-cols-1 row-cols-sm-2'>
+                {dataHeroBox.map((data) => {
+                  return (
+                    <Col data-aos='flip-right' key={data.id}>
+                      <HeroBox {...data} />
+                    </Col>
+                  );
+                })}
+              </Row>
 
-            <div className='btn-start-learn'>
-              شروع یادگیری <FaGooglePlay className='icon' />
-            </div>
-          </Col>
-        </Row>
+              <div className='btn-start-learn'>
+                شروع یادگیری <FaGooglePlay className='icon' />
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </Container>
-    </Container>
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+        <path
+          fill='#00a9ff'
+          d='M0 224l30-32c30-32 90-96 150-101.3 60-5.7 120 48.3 180 90.6 60 42.7 120 74.7 180 69.4 60-5.7 120-47.7 180-69.4C780 160 840 160 900 144c60-16 120-48 180-26.7 60 21.7 120 95.7 180 90.7s120-91 150-133.3l30-42.7V0H0z'
+        ></path>
+      </svg>
+    </>
   );
 };
 export default memo(Hero);
