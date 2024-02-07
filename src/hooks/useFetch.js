674526dc84c17fea9795b7304e66ec9565ad44be
Iamhhp <Iamhhp2020@gmail.com>
2024-02-07 13:18:27 +0330

    update useFetch add response and responseState

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const useFetch = (url) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({ responseStatus: '', response: [] });
   const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
@@ -9,16 +9,16 @@ const useFetch = (url) => {
       .then((response) => {
         if (response.status === 200) {
           response.json().then((json) => {
-            setData(json);
+            setData((prevState) => ({ ...prevState, responseStatus: `receivedData!`, response: json }));
             setIsPending(false);
           });
         } else {
-          setData({ responseStatus: `${response.statusText} ${response.status}` });
+          setData((prevState) => ({ ...prevState, responseStatus: `${response.statusText} ${response.status}`, response: [] }));
           setIsPending(false);
         }
       })
       .catch((err) => {
-        setData({ responseStatus: 'Failed to Receive Data!' });
+        setData((prevState) => ({ ...prevState, responseStatus: 'Failed to Receive Data!', response: [] }));
         setIsPending(false);
       });
   }, []);
