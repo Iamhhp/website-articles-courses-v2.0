a4c87cdcae428efc741c4b43148add9717553759
Iamhhp <Iamhhp2020@gmail.com>
2024-02-16 19:56:21 +0330

    add Route login and sign to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import EditCreateArticle from './pages/EditCreateArticle/EditCreateArticle';
 import Articles from './pages/Articles/Articles';
 import Courses from './pages/Courses/Courses';
 import Course from './pages/Course/Course';
+import Login from './pages/Login/Login';
 
 const App = () => {
   return (
@@ -19,7 +20,10 @@ const App = () => {
           <Route path='/Articles' element={<Articles />} />
           <Route path='/Courses' element={<Courses />} />
           <Route path='/Course/:idCourse' element={<Course />} />
+          <Route path='/Login' element={<Login />} />
         </Route>
+
+        <Route path='*' element={<h1>Not Found!</h1>} />
       </Routes>
     </BrowserRouter>
   );
