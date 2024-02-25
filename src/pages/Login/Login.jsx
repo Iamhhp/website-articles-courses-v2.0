03b898c67edc10245d4ab357e2f7259f8d9db46c
Iamhhp <Iamhhp2020@gmail.com>
2024-02-25 18:31:21 +0330

    complete Component Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,68 +1,40 @@
-import { useState } from 'react';
 import './Login.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import FormLogin from '../../components/FormLogin/FormLogin';
+import FormSign from '../../components/FormSign/FromSign';
+import FromForgetPass from '../../components/FormForgetPass/FromForgetPass';
 
 const Login = () => {
-  const [isShowLoginForm, setIsShowLoginForm] = useState(false);
+  const navigte = useNavigate();
+  const [isShowFormLogin, setIsShowFormLogin] = useState(true);
+  const [isShowForgetPass, setIsShowForgetPass] = useState(false);
 
+  ////////////////////////////////////////////////////////////////////////////////////// Function Handler Select From Login OR Sign
   const changeHandlerRadioLoginSign = () => {
-    setIsShowLoginForm((prevState) => !prevState);
+    setIsShowFormLogin((prevState) => !prevState);
   };
 
   return (
-    <div className='container-login'>
-      <div className='btns-login'>
-        <input type='radio' name='login-sign' id='login' defaultChecked onChange={changeHandlerRadioLoginSign} />
-        <label htmlFor='login' className='login'>
-          ورود
-        </label>
+    <div className='container-user-login'>
+      {isShowForgetPass ? (
+        <FromForgetPass setIsShowForgetPass={setIsShowForgetPass} />
+      ) : (
+        <div className='container-form-login-sign'>
+          <div className='btns-switch-login'>
+            <input type='radio' name='login-sign' id='login' defaultChecked onChange={changeHandlerRadioLoginSign} />
+            <label htmlFor='login' className='login'>
+              ورود
+            </label>
 
-        <input type='radio' name='login-sign' id='sign' onChange={changeHandlerRadioLoginSign} />
-        <label htmlFor='sign'>ثبت نام</label>
-        <div className='slider' />
-      </div>
+            <input type='radio' name='login-sign' id='sign' onChange={changeHandlerRadioLoginSign} />
+            <label htmlFor='sign'>ثبت نام</label>
+            <div className='slider' />
+          </div>
 
-      <div className={`forms ${isShowLoginForm && 'form-login-show'}`}>
-        <div className='form-login'>
-          <div className='title'>ورود</div>
-          <label className='lbl-username' htmlFor='username'>
-            نام کاربری
-          </label>
-          <input className='input-username' type='text' id='username' placeholder='یوزرنیم خود را وارد کنید' />
-
-          <label className='lbl-pass' htmlFor='password'>
-            پسورد
-          </label>
-          <input className='input-pass' type='password' id='password' placeholder='پسورد خود را وارد کنید' />
-          <a href='' className='forget-pass'>
-            رمز فراموش کرده اید؟
-          </a>
-
-          <label className='remember' htmlFor='remember'>
-            <input type='checkbox' id='remember' />
-            مرا به خاطر بسپار!
-          </label>
-          <button className='btn-login-sign'>ورود</button>
+          <div className='forms'>{isShowFormLogin ? <FormLogin setIsShowForgetPass={setIsShowForgetPass} /> : <FormSign setIsShowFormLogin={setIsShowFormLogin} />}</div>
         </div>
-        <div className='form-sign'>
-          <div className='title'>ثبت نام</div>
-          <label className='lbl-username' htmlFor='username'>
-            نام کاربری
-          </label>
-          <input className='input-username' type='text' id='username' placeholder='یوزرنیم خود را وارد کنید' />
-
-          <label className='lbl-pass' htmlFor='password'>
-            پسورد
-          </label>
-          <input className='input-pass' type='password' id='password' placeholder='پسورد خود را وارد کنید' />
-
-          <label className='lbl-rpt-pass' htmlFor='rpt-password'>
-            تکرار پسورد
-          </label>
-          <input className='input-pass' type='rpt-password' id='rpt-password' placeholder='پسورد خود را تکرار کنید' />
-
-          <button className='btn-login-sign'>ثبت نام</button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
