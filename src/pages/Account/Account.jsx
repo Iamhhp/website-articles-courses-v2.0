087540f08e76797cb9dd09cef0ca919b77323dd1
Iamhhp <Iamhhp2020@gmail.com>
2024-03-10 11:19:23 +0330

    ceate account

---
+++ src/pages/Account/Account.jsx
@@ -0,0 +1,99 @@
+import './Account.css';
+import { NavLink, Route, Routes, useNavigate } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+import DetailsAccount from '../DetailsAccount/DetailsAccount';
+import CoursesAccount from '../CoursesAccount/CoursesAccount';
+import { useChangeUserDataContext, useUserDataContext } from '../../context/DataContext';
+import Swal from 'sweetalert2';
+import { ACTION_TYPE } from '../../context/hooks/useUserDataReducer';
+import { useState } from 'react';
+
+const Account = () => {
+  const navigate = useNavigate();
+  const changeUserData = useChangeUserDataContext();
+  const { boughtCards, selectedCards } = useUserDataContext();
+
+  const clickHandlerExitAccount = () => {
+    Swal.fire({
+      icon: 'question',
+      text: 'آیا از سایت خارج می شوید؟',
+      showCancelButton: true,
+      cancelButtonText: 'خیر',
+      cancelButtonColor: 'green',
+      showConfirmButton: true,
+      confirmButtonText: 'بله',
+      confirmButtonColor: 'red',
+    })
+      .then((result) => {
+        if (result.isConfirmed) {
+          changeUserData(ACTION_TYPE.LOGOUT, {});
+          window.sessionStorage.removeItem('userData');
+          window.localStorage.removeItem('userData');
+          navigate('/home');
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <Container className='container-account'>
+      <Row>
+        <Col className='col-12 col-lg-4 col-xl-3'>
+          <div className='sec-r'>
+            <div className='img'>
+              <img src='https://secure.gravatar.com/avatar/957e7f28ff111d0eb26e0fe51d9650d5?s=80&d=mm&r=g' alt='' />
+            </div>
+            <div className='name-user'>حسن پور</div>
+            <ul className='items-menu-account'>
+              <Row className='row-cols-3 row-cols-lg-1 '>
+                <Col>
+                  <li>
+                    <NavLink tabIndex='-1' to='/account/courses/selected'>
+                      سبد خرید
+                      {selectedCards.length !== 0 && <span className='number-courses'>{selectedCards.length}</span>}
+                    </NavLink>
+                  </li>
+                </Col>
+
+                <Col>
+                  <li>
+                    <NavLink tabIndex='-1' to='/account/courses/bought'>
+                      دوره های خریداری شده
+                      {boughtCards.length !== 0 && <span className='number-courses'>{boughtCards.length}</span>}
+                    </NavLink>
+                  </li>
+                </Col>
+
+                <Col>
+                  <li>
+                    <NavLink tabIndex='-1' to='/account/details'>
+                      اطلاعات حساب کاربری
+                    </NavLink>
+                  </li>
+                </Col>
+
+                <Col>
+                  <li>
+                    <button type='button' className='btn-exit' tabIndex='-1' onClick={clickHandlerExitAccount}>
+                      خروج از سایت
+                    </button>
+                  </li>
+                </Col>
+              </Row>
+            </ul>
+          </div>
+        </Col>
+
+        <Col className='col-12 col-lg-8 col-xl-9'>
+          <div className='sec-l'>
+            <Routes>
+              <Route path='Details' element={<DetailsAccount />} />
+              <Route path='Courses/:subRoute' element={<CoursesAccount />} />
+            </Routes>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default Account;
