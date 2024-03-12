ae8d1239ff50e347389cffb3d2f68aa403fbdbb1
Iamhhp <Iamhhp2020@gmail.com>
2024-03-12 15:26:22 +0330

    add DocJs to userDataReducer and add delay 1s to get data

--- src/context/hooks/useUserDataReducer.js
+++ src/context/hooks/useUserDataReducer.js
@@ -1,6 +1,17 @@
-import { useReducer } from 'react';
+import { useCallback, useReducer } from 'react';
 
+/**
+ *@description Other Property => payLoad = str
+ * @property {LOGOUT} => payLoad = Empty
+ * @property {ADD_CARD_SELECTED} => payLoad = obj-{}
+ * @property {DEL_CARD_SELECTED} => payLoad = number-IdCourse
+ * @property {DEL_ALL_CARD_SELECTED} => payLoad = Empty
+ * @property {ADD_CARD_BOUGHT} => payLoad = array-[]
+ * @property {PATCH_DATA} => payLoad = obj-{}
+ * @property {PUT_DATA} => payLoad = obj-{}
+ */
 export const ACTION_TYPE = {
+  // stay in login
   LOGOUT: 0x0,
   CNG_USERNAME: 0x1,
   CNG_PASS: 0x2,
@@ -50,12 +61,15 @@ const useUserDataReducer = () => {
         return { ...state, selectedCards: [...state.selectedCards, action.payLoad] };
       case ACTION_TYPE.DEL_CARD_SELECTED:
         return { ...state, selectedCards: state.selectedCards.filter((card) => card.id !== action.payLoad) };
+
       case ACTION_TYPE.DEL_ALL_CARD_SELECTED:
         return { ...state, selectedCards: [] };
 
       //////////////////////////////////////////////////////////////// Actions of Cards Bought
       case ACTION_TYPE.ADD_CARD_BOUGHT:
         return { ...state, boughtCards: [...state.boughtCards, ...action.payLoad] };
+
+      //////////////////////////////////////////////////////////////// Actions of update All data
       case ACTION_TYPE.PATCH_DATA:
         return { ...state, ...action.payLoad };
       case ACTION_TYPE.PUT_DATA:
@@ -67,9 +81,9 @@ const useUserDataReducer = () => {
 
   const [stateUserData, dispatch] = useReducer(userDataReducer, initialState);
 
-  const changeUserData = (actionType, actionPayLoad) => {
+  const changeUserData = useCallback((actionType, actionPayLoad) => {
     dispatch({ type: actionType, payLoad: actionPayLoad });
-  };
+  }, []);
 
   return {
     stateUserData,
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,22 +5,25 @@ const useFetch = (url) => {
   const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        if (response.status === 200) {
-          response.json().then((json) => {
-            setData((prevState) => ({ ...prevState, responseStatus: `dataReceived!`, response: json }));
+    window.setTimeout(() => {
+      // delay for more showing component Loading
+      fetch(url)
+        .then((response) => {
+          if (response.status === 200) {
+            response.json().then((json) => {
+              setData((prevState) => ({ ...prevState, responseStatus: `dataReceived!`, response: json }));
+              setIsPending(false);
+            });
+          } else {
+            setData((prevState) => ({ ...prevState, responseStatus: `${response.statusText} ${response.status}`, response: [] }));
             setIsPending(false);
-          });
-        } else {
-          setData((prevState) => ({ ...prevState, responseStatus: `${response.statusText} ${response.status}`, response: [] }));
+          }
+        })
+        .catch((err) => {
+          setData((prevState) => ({ ...prevState, responseStatus: 'Failed to Receive Data!', response: [] }));
           setIsPending(false);
-        }
-      })
-      .catch((err) => {
-        setData((prevState) => ({ ...prevState, responseStatus: 'Failed to Receive Data!', response: [] }));
-        setIsPending(false);
-      });
+        });
+    }, 1000);
   }, []);
 
   return [data, isPending];
