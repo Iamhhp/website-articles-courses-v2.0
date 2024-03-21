2982ba8d2e3bf8cbe75d79cb1809636e3f93f3e3
Iamhhp <Iamhhp2020@gmail.com>
2024-03-21 20:24:37 +0330

    edit notification-add button close and time out bar

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,7 +1,8 @@
-import { memo, useEffect, useRef } from 'react';
 import './Notification.css';
+import { memo, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteNotification } from '../../context/Redux/notificationDataSlice';
+import { IoIosCloseCircleOutline } from 'react-icons/io';
 
 const Notification = ({ id, backgroundColor, msg, i }) => {
   const timer = { id: 0 };
@@ -12,7 +13,8 @@ const Notification = ({ id, backgroundColor, msg, i }) => {
     window.setTimeout(() => {
       window.setTimeout(() => {
         if (notification.current) {
-          notification.current.style.top = `${i * 35 + 90}px`;
+          notification.current.style.top = `${i * 40 + 90}px`;
+          notification.current.children[1].style.width = '0px';
 
           timer.id = window.setTimeout(() => {
             if (notification.current) {
@@ -32,9 +34,24 @@ const Notification = ({ id, backgroundColor, msg, i }) => {
     };
   }, []);
 
+  const closeHandlerNotification = () => {
+    if (notification.current) {
+      notification.current.style.top = '';
+
+      window.setTimeout(() => {
+        setNotification(deleteNotification(id));
+      }, 500);
+    }
+  };
+
   return (
     <div className='notification' style={{ backgroundColor }} ref={notification}>
-      {msg}
+      <div className='message'>
+        {msg}
+        <IoIosCloseCircleOutline className='icon-close-notification' onClick={closeHandlerNotification} />
+      </div>
+
+      <div className='time-out-bar' />
     </div>
   );
 };
